fb1b8198bd621f1f690c90604f85cdb46aaa65ae
PureDalek <puredalek@gmail.com>
2024-07-01 08:26:58 +0300

    daily

--- utills/git_update.py
+++ utills/git_update.py
@@ -1,16 +1,32 @@
 import os
+import sys
 import easygui as eg
 from dotenv import load_dotenv
 
-# Ask the user for the commit message using a pop-up dialog
-commit_message = eg.enterbox("Enter a commit message:", "Git Commit")
-load_dotenv()
-FOLDER_PATH = os.getenv("FOLDER_PATH")
-if commit_message:
-    # Execute Git commands
-    os.chdir(FOLDER_PATH)
-    os.system('git add .')
-    os.system(f'git commit -m "{commit_message}"')
-    os.system('git push')
-else:
-    eg.msgbox("No commit message entered. Operation canceled.", "Git Commit")
+def execute_git_commands(commit_message):
+    """Execute git commands to add, commit, and push changes."""
+    load_dotenv()
+    folder_path = os.getenv("FOLDER_PATH")
+    if folder_path:
+        os.chdir(folder_path)
+        os.system('git add .')
+        os.system(f'git commit -m "{commit_message}"')
+        os.system('git push')
+    else:
+        print("FOLDER_PATH environment variable is not set. Operation canceled.")
+
+def main():
+    # Check if a commit message is provided as a command-line argument
+    if len(sys.argv) > 2 and sys.argv[1] == "-m":
+        commit_message = sys.argv[2]
+    else:
+        # Ask the user for the commit message using a pop-up dialog
+        commit_message = eg.enterbox("Enter a commit message:", "Git Commit")
+    
+    if commit_message:
+        execute_git_commands(commit_message)
+    else:
+        eg.msgbox("No commit message entered. Operation canceled.", "Git Commit")
+
+if __name__ == "__main__":
+    main()
