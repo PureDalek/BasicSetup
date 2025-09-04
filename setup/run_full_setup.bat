6bbfceccb260346b077df6a3fb05d722e9bd28e4
PureDalek <puredalek@gmail.com>
2025-09-04 15:39:54 +0300

    command

--- setup/run_full_setup.bat
+++ setup/run_full_setup.bat
@@ -25,10 +25,10 @@ if '%errorlevel%' NEQ '0' (
     where choco >nul 2>nul
     if %errorlevel% NEQ 0 (
         echo Chocolatey not found. Installing Chocolatey...
-        powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-            "Set-ExecutionPolicy Bypass -Scope Process -Force; ^
-            [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; ^
+        powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; 
+            [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
             iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
+            
     ) else (
         echo Chocolatey is already installed.
     )
